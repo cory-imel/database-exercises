f8794aeb86413c2ecba6a4233affb9727b5527a1
Cory Imel <cory.imel@gmail.com>
2017-12-11 14:06:27 -0600

    added update/delete exercises

---
+++ delete_exercises.sql
@@ -0,0 +1,10 @@
+SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+
+
+SELECT * FROM albums WHERE genre LIKE 'Disco';
+DELETE FROM albums WHERE genre LIKE 'Disco';
+
+
+SELECT * FROM albums WHERE artist LIKE 'Meatloaf';
+DELETE FROM albums WHERE artist LIKE 'Meatloaf';
