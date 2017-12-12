39c282bfc0e7241a64663534467a635fd73ac1bb
Cory Imel <cory.imel@gmail.com>
2017-12-12 10:28:58 -0600

    finished where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+
+SELECT first_name,gender
+FROM employees
+WHERE first_name ='Irena'
+      AND gender = 'M'
+      OR first_name ='Vidya'
+      AND gender = 'M'
+      OR first_name ='Maya'
+      AND gender = 'M';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
+
+SELECT first_name,last_name,hire_date,birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+      OR hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
