9fb0e74fd1188936251e0bd76b27c83854431a44
Cory Imel <cory.imel@gmail.com>
2017-12-12 12:16:50 -0600

    added limit and order exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE (first_name ='Irena'
+       OR first_name ='Vidya'
+       OR first_name ='Maya')
+      AND gender = 'M'
+ORDER BY last_name DESC , first_name DESC ;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+      AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date ASC , hire_date DESC ;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
