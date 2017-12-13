7d50d983bf5c77f0cea9aaf2bb579b1516f2834f
Cory Imel <cory.imel@gmail.com>
2017-12-13 17:05:37 -0600

    updated checer

--- exercises_check.sh
+++ exercises_check.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-files=(albums_migration.sql            database-exercises.iml          functions_exercises.sql         limit_exercises.sql             subqueries_exercises.sql
-       albums_seeder.sql               delete_exercises.sql            group_by_exercises.sql          order_by_exercises.sql          update_exercises.sql
-       aliases_exercises.sql           exercises_check.sh              join_exercises.sql              select_exercise.sql             where_exercises.sql)
+files=(    albums_migration.sql
+           albums_seeder.sql
+           aliases_exercises.sql
+           delete_exercises.sql
+           functions_exercises.sql
+           group_by_exercises.sql
+           join_exercises.sql
+           limit_exercises.sql
+           order_by_exercises.sql
+           select_exercises.sql
+           subqueries_exercises.sql
+           update_exercises.sql
+           where_exercises.sql )
 
 for file in "${files[@]}"
 do
@@ -9,4 +19,6 @@ do
     then echo "$file is missing";
     fi
 done
+
+
 
