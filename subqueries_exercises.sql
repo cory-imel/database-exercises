fa7e2ab27c46ac1bc49d59e2ef65b7d5f10e11e3
Cory Imel <cory.imel@gmail.com>
2017-12-15 09:59:58 -0600

    added subquries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date = (SELECT hire_date FROM employees
+                    WHERE emp_no = 101010);
+
+SELECT title
+FROM titles
+WHERE emp_no IN (SELECT emp_no FROM employees
+                     WHERE first_name = 'Aamod');
+
+SELECT first_name,last_name
+FROM employees
+WHERE gender = 'F' AND emp_no IN (SELECT emp_no FROM dept_manager
+                                  WHERE to_date = "9999-01-01");
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (SELECT dept_no FROM dept_manager
+                   WHERE to_date = "9999-01-01" AND emp_no IN (SELECT emp_no FROM employees WHERE gender = 'F'));
